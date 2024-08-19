11e1023b3b47ee4e77a98efc935e15d243c98f7e
isaacmaffeis <isaacmaffeis@gmail.com>
2024-08-19 14:09:58 +0200

    updating .gitignore

--- evoservice/scripts/gen_evosuite.sh
+++ evoservice/scripts/gen_evosuite.sh
@@ -2,8 +2,3 @@
 
 echo "# gen_evosuite.sh"
 echo "## Generating test suite with EvoSuite"
-
-echo "Running evosuite-1.2.0 on registroDiCassa.RegistroDiCassa..."
-java -jar evosuite-1.2.0.jar -class org.evoservice.registroDiCassa.RegistroDiCassa
-
-echo "gen_evosuite.sh process completed."
