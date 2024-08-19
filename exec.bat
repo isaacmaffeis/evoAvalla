ab10399a3b8c741e1d6eaee42425f5f30c7d7720
isaacmaffeis <isaacmaffeis@gmail.com>
2024-08-19 14:00:26 +0200

    added bat exec

---
+++ exec.bat
@@ -0,0 +1,26 @@
+echo "#EvoAvalla"
+echo "## "
+
+echo "Updating the images..."
+docker compose pull
+
+echo "Starting the containers..."
+docker compose up
+
+echo "Cleaning folders..."
+del /Q .\evoservice\target\*
+for /d %%i in (.\evoservice\target\*) do rmdir /S /Q "%%i"
+
+echo "Running asmetal2java..."
+docker compose run --rm asmetal2java -input ./input/RegistroDiCassa.asm
+
+echo "Running evoservice..."
+docker compose run --rm evoservice python3 ./scripts/retrieve_input.py
+
+docker compose run --rm evoservice ./scripts/mvn_setup.sh
+
+docker compose run --rm evoservice python3 scripts/gen_evosuite_sh.py
+
+docker compose run --rm evoservice ./scripts/gen_evosuite.sh
+
+docker compose run --rm evoservice ./scripts/tests.sh
