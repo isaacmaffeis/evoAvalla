7363116b14186daecb493cd00103423baf8368c3
isaacmaffeis <isaacmaffeis@gmail.com>
2024-09-18 18:15:29 +0200

    reorganizing the folder

--- evoservice/scripts/gen_evosuite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "# gen_evosuite.sh"
-echo "## Generating test suite with EvoSuite"
-
-echo "Running evosuite-1.0.6 on registroDiCassa.RegistroDiCassa..."
-java -jar evosuite-1.0.6.jar -class org.evoservice.registroDiCassa.RegistroDiCassa
-
-echo "gen_evosuite.sh process completed."
--- exec.bat
+++ exec.bat
@@ -16,7 +16,7 @@ if "%input_file%"=="" (
 for %%f in ("%input_file%") do set "input_file_no_ext=%%~nf"
 
 echo "Found %input_file_no_ext%.asm in %input_dir% with path %input_file%"
-move "%input_file%" ".\asmetal2java\input"
+move "%input_file%" ".\services\asmetal2java\input"
 
 echo "Updating the images..."
 docker compose pull -q
@@ -25,12 +25,12 @@ echo "Starting the containers..."
 docker compose up -d --build
 
 echo "Cleaning folders..."
-del /Q .\evoservice\target\*
-for /d %%i in (.\evoservice\target\*) do rmdir /S /Q "%%i"
+del /Q .\services\evoservice\target\*
+for /d %%i in (.\services\evoservice\target\*) do rmdir /S /Q "%%i"
 
 echo "Running asmetal2java..."
 docker compose run --rm asmetal2java -input "./input/%input_file_no_ext%.asm"
-move ".\evoservice\input\StepFunctionArgs.txt" ".\javaToAvalla\input"
+move ".\services\evoservice\input\StepFunctionArgs.txt" ".\services\javaToAvalla\input"
 
 echo "Running evoservice..."
 docker compose run --rm evoservice python3 ./scripts/retrieve_input.py
