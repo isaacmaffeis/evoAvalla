db115a106b0be32932ca2d422d90caffbb701798
isaacmaffeis <isaacmaffeis@gmail.com>
2024-09-18 17:51:07 +0200

    updated the script batch

--- exec.bat
+++ exec.bat
@@ -1,14 +1,22 @@
 echo "#EvoAvalla"
 echo "## "
 
-set /p input_file="Digit the asm file name <asmName>.asm (without .asm): "
-
-if not exist .\input\%input_file%.asm (
-    echo "Error: File %input_file% not exists. Stopping the execution..."
+echo "Searching the .asm input file"
+set "input_dir=.\input"
+set "input_file="
+for %%f in ("%input_dir%\*.asm") do (
+    set "input_file=%%f"
+    goto :found
+)
+:found
+if "%input_file%"=="" (
+    echo "Error: No .asm file found in %input_dir%. Stopping execution."
     exit /b
 )
+for %%f in ("%input_file%") do set "input_file_no_ext=%%~nf"
 
-move ".\input\%input_file%.asm" ".\asmetal2java\input"
+echo "Found %input_file_no_ext%.asm in %input_dir% with path %input_file%"
+move "%input_file%" ".\asmetal2java\input"
 
 echo "Updating the images..."
 docker compose pull -q
@@ -21,7 +29,7 @@ del /Q .\evoservice\target\*
 for /d %%i in (.\evoservice\target\*) do rmdir /S /Q "%%i"
 
 echo "Running asmetal2java..."
-docker compose run --rm asmetal2java -input "./input/%input_file%.asm"
+docker compose run --rm asmetal2java -input "./input/%input_file_no_ext%.asm"
 move ".\evoservice\input\StepFunctionArgs.txt" ".\javaToAvalla\input"
 
 echo "Running evoservice..."
@@ -36,7 +44,7 @@ docker compose run --rm evoservice ./scripts/gen_evosuite.sh
 docker compose run --rm evoservice ./scripts/tests.sh
 
 echo "Running javatoavalla"
-docker compose run --rm javatoavalla -input "./input/%scenario_file%_ASM_ESTest.java" -clean true
+docker compose run --rm javatoavalla -input "./input/%input_file_no_ext%_ASM_ESTest.java" -clean true
 
 echo "stop and remove containers"
 docker compose down
