26d968b89f98ddd34f80e40c71c7ce2776f99c4d
isaacmaffeis <isaacmaffeis@gmail.com>
2024-09-18 14:04:30 +0200

    added new image

--- exec.bat
+++ exec.bat
@@ -1,6 +1,15 @@
 echo "#EvoAvalla"
 echo "## "
 
+set /p input_file="Digit the asm file name <asmName>.asm (without .asm): "
+
+if not exist .\input\%input_file%.asm (
+    echo "Error: File %input_file% not exists. Stopping the execution..."
+    exit /b
+)
+
+move ".\input\%input_file%.asm" ".\asmetal2java\input"
+
 echo "Updating the images..."
 docker compose pull -q
 
@@ -12,18 +21,22 @@ del /Q .\evoservice\target\*
 for /d %%i in (.\evoservice\target\*) do rmdir /S /Q "%%i"
 
 echo "Running asmetal2java..."
-docker compose run --rm asmetal2java -input ./input/RegistroDiCassa.asm
+docker compose run --rm asmetal2java -input "./input/%input_file%.asm"
+move ".\evoservice\input\StepFunctionArgs.txt" ".\javaToAvalla\input"
 
 echo "Running evoservice..."
 docker compose run --rm evoservice python3 ./scripts/retrieve_input.py
 
 docker compose run --rm evoservice ./scripts/mvn_setup.sh
 
-docker compose run --rm evoservice python3 scripts/gen_evosuite_sh.py
+docker compose run --rm evoservice python3 scripts/gen_evosuite_sh.py -criterion LINE:BRANCH -Dminimize=true -Dassertion_strategy=all
 
 docker compose run --rm evoservice ./scripts/gen_evosuite.sh
 
 docker compose run --rm evoservice ./scripts/tests.sh
+
+echo "Running javatoavalla"
+docker compose run --rm javatoavalla -input "./input/%scenario_file%_ASM_ESTest.java" -clean true
 
 echo "stop and remove containers"
 docker compose down
